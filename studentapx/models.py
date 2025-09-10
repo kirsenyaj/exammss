7e93c39fdf933e9a3d699cd412e85620344ba1d6
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-09-10 11:43:27 +0800

    Initial Commit

--- studentapx/models.py
+++ studentapx/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class StudentRecord(models.Model):
-    student_id = models.CharField(max_length=20, unique=True)
+    student_id = models.CharField(max_length=25, unique=True)
     full_name = models.CharField(max_length=100)
     course = models.CharField(max_length=50)
     year_level = models.IntegerField()
